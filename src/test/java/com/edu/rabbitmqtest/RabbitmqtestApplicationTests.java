76a3eb1b4bdf04614f27451a9d79df145a6de8c0
ruanxingke <rxingke@gmail.com>
2019-11-13 21:53:45 -0600

    first version

---
+++ src/test/java/com/edu/rabbitmqtest/RabbitmqtestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edu.rabbitmqtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RabbitmqtestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
